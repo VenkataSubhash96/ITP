1247297451e396199c9cd1a168a8795689b4ba1d
Venkata Subhash <subhashbharadwaj96@gmail.com>
2018-09-18 17:36:14 +0530

    Rename get_content to receive_file

--- lib/itp.rb
+++ lib/itp.rb
@@ -5,20 +5,20 @@ class Itp
     IPFS::Client.default
   end
 
-  def self.add_file(file_name)
+  def self.share_file(file_name)
     link = ipfs_client.add(file_name)
     link.hashcode
   rescue HTTP::ConnectionError
-    "Cannot Add file. IPFS daemon server not running. Run 'ipfs daemon'"
+    "Cannot Add file. IPFS daemon server not running. Run 'ipfs daemon'. Make sure you have ipfs gem installed."
   end
 
-  def self.view_content(hash_code)
+  def self.receive_file(hash_code)
     content = ipfs_client.cat(hash_code)
     File.open('received.txt', 'w') do |output_file|
       output_file.write(content)
     end
     'Received file'
   rescue HTTP::ConnectionError
-    "Cannot receive file. IPFS daemon server not running. Run 'ipfs daemon'"
+    "Cannot receive file. IPFS daemon server not running. Run 'ipfs daemon'. Make sure you have ipfs gem installed."
   end
 end
