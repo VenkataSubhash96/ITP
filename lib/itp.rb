b9bb136a261208e04ccc42bfe88faa494589925d
Venkata Subhash <subhashbharadwaj96@gmail.com>
2018-09-18 16:25:36 +0530

    Rescue Http:ConnectionError in itp.rb

--- lib/itp.rb
+++ lib/itp.rb
@@ -8,9 +8,13 @@ class Itp
   def self.add_file(file_name)
     link = ipfs_client.add(file_name)
     link.hashcode
+  rescue HTTP::ConnectionError
+    "Cannot Add file. IPFS daemon server not running. Run 'ipfs daemon'"
   end
 
   def self.view_content(hash_code)
     ipfs_client.cat(hash_code)
+  rescue HTTP::ConnectionError
+    "Cannot receive file. IPFS daemon server not running. Run 'ipfs daemon'"
   end
 end
